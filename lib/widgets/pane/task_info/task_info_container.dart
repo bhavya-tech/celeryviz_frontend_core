d879d2995b2191e655e1b82397c5c521a4732c04
bhavya-tech <64970880+bhavya-tech@users.noreply.github.com>
2025-10-27 19:39:09 +0530

    listen to pane bloc and update the task info

--- lib/widgets/pane/task_info/task_info_container.dart
+++ lib/widgets/pane/task_info/task_info_container.dart
@@ -1,3 +1,6 @@
+import 'package:celeryviz_frontend_core/states/pane/pane_bloc.dart';
+import 'package:celeryviz_frontend_core/states/pane/pane_state.dart';
+import 'package:celeryviz_frontend_core/states/task_info/task_info_event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:celeryviz_frontend_core/constants.dart';
@@ -10,30 +13,47 @@ class TaskInfoContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TaskInfoBloc, TaskInfoState>(builder: (context, state) {
-      return AnimatedContainer(
-        padding: const EdgeInsets.all(8),
-        margin: const EdgeInsets.all(8),
-        alignment: Alignment.centerRight,
-        duration: const Duration(milliseconds: 600),
-        curve: Curves.linear,
-        transform: Matrix4.translationValues(
-          state.isVisible
-              ? MediaQuery.of(context).size.width - taskInfoAreaWidth - 128
-              : MediaQuery.of(context).size.width,
-          0,
-          0,
-        ),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-        ),
-        width: taskInfoAreaWidth,
-        child: (state.taskInfo == null)
-            ? const Placeholder()
-            : TaskInfoArea(taskInfo: state.taskInfo!),
-      );
-    });
+    return BlocListener<PaneBloc, PaneState>(
+      listener: (context, paneState) {
+        if (paneState.isStarted) {
+          final currentTaskInfo = context.read<TaskInfoBloc>().state.taskInfo;
+
+          if (currentTaskInfo != null) {
+            final newTaskInfo =
+                paneState.data.tasks[currentTaskInfo.taskId]?.taskInfo;
+
+            if (newTaskInfo != null) {
+              context.read<TaskInfoBloc>().add(TaskInfoUpdated(newTaskInfo));
+            }
+          }
+        }
+      },
+      child:
+          BlocBuilder<TaskInfoBloc, TaskInfoState>(builder: (context, state) {
+        return AnimatedContainer(
+          padding: const EdgeInsets.all(8),
+          margin: const EdgeInsets.all(8),
+          alignment: Alignment.centerRight,
+          duration: const Duration(milliseconds: 600),
+          curve: Curves.linear,
+          transform: Matrix4.translationValues(
+            state.isVisible
+                ? MediaQuery.of(context).size.width - taskInfoAreaWidth - 128
+                : MediaQuery.of(context).size.width,
+            0,
+            0,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          width: taskInfoAreaWidth,
+          child: (state.taskInfo == null)
+              ? const Placeholder()
+              : TaskInfoArea(taskInfo: state.taskInfo!),
+        );
+      }),
+    );
   }
 }
